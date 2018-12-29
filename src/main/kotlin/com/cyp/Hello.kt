09e5adbf7bf047083365f8f719f21e8b138232e3
superkid0125 <superkid0125@gmail.com>
2018-12-29 14:00:13 +0800

    Initial commit

---
+++ src/main/kotlin/com/cyp/Hello.kt
@@ -0,0 +1,13 @@
+package com.cyp
+
+fun main(args:Array<String>){
+//    println("Hello Kotlin!!!");
+    val h = Human();
+    h.hello();
+}
+
+class Human{
+    fun hello(){
+        println("Hello Kotlin!!!");
+    }
+}
